5bfda398509ac0473092d40ff25bb7a8f906cfc1
mgckaled <marcelgckaled@gmail.com>
2023-02-05 22:29:44 -0300

    feat: end Sidebarcomponent

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react'
+
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
@@ -9,12 +11,17 @@ export function Sidebar() {
 			/>
 
 			<div className={styles.profile}>
-				<strong>Diego Fernandes</strong>
+				<img className={styles.avatar} src="https://github.com/mgckaled.png" />
+
+				<strong>Marcel Kaled</strong>
 				<span>Web Developer</span>
 			</div>
 
 			<footer>
-				<a href="#">Editar seu perfil</a>
+				<a href="#">
+					<PencilLine size={20} />
+					Editar seu perfil
+				</a>
 			</footer>
 		</aside>
 	)
