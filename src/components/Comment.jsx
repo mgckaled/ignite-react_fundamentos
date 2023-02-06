256401c31a37958932aad77a3efbae6a1aac50d0
mgckaled <marcelgckaled@gmail.com>
2023-02-06 18:55:54 -0300

    styles: define coComment component styles

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -5,24 +5,32 @@ import styles from './Comment.module.css'
 export function Comment() {
 	return (
 		<div className={styles.comment}>
-			<img src="https://github.com/diego3g.png" alt="" />
+			<img src="https://github.com/mgckaled.png" alt="avatar do usuário" />
 
 			<div className={styles.commentBox}>
 				<div className={styles.commentContent}>
 					<header>
 						<div className={styles.authorAndTime}>
-							<strong>Diego Fernandes</strong>
-							<time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">
+							<strong>Marcel Kaled</strong>
+							<time
+								title="06 de Fevereiro às 15:10h"
+								dateTime="2023-02-06 15:10:00"
+							>
 								Cerca de 1h atrás
 							</time>
 						</div>
 
 						<button title="Deletar comentário">
-							<Trash size={20} />
+							<Trash size={24} />
 						</button>
 					</header>
 
-					<p>Muito bom Devon, parabéns!! 👏👏</p>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+						libero earum eius pariatur at molestiae fugit voluptatibus nam cum
+						sunt deserunt ut in laudantium optio, quibusdam alias odio! Odit,
+						vitae. 👏👏
+					</p>
 				</div>
 
 				<footer>
