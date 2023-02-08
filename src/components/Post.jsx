f18ead37880f5a5a3c03a2a2a5948023de880649
mgckaled <marcelgckaled@gmail.com>
2023-02-07 23:58:56 -0300

    feat: add key to elements

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -35,6 +35,14 @@ export function Post({ author, publishedAt, content }) {
 		setNewCommentText(event.target.value)
 	}
 
+	function deleteComment(commentToDelete) {
+		const commentsWithoutDeletedOne = comments.filter(comment => {
+			return comment !== commentToDelete
+		})
+
+		setComments(commentsWithoutDeletedOne)
+	}
+
 	return (
 		<article className={styles.post}>
 			<header>
@@ -83,7 +91,13 @@ export function Post({ author, publishedAt, content }) {
 			</form>
 			<div className={styles.commentList}>
 				{comments.map(comment => {
-					return <Comment key={comment} content={comment} />
+					return (
+						<Comment
+							key={comment}
+							content={comment}
+							onDeleteComment={deleteComment}
+						/>
+					)
 				})}
 			</div>
 		</article>
